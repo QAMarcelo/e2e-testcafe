e59efa16443448eaa41b6ec105f76a63e93ff7e9
msanabria-wdg <msanabria@wdgcorp.com>
2022-11-10 19:05:18 -0400

    PD-932 Removed duplicated test

--- tests/smoke/login.ts
+++ tests/smoke/login.ts
@@ -72,32 +72,3 @@ test
         //Verify the table has data displayed
         await t.expect(Selector('tr.k-grid-norecords').exists).notOk("There is no data retreived from the DB!");
     });
-
-
-/* 
-    [SMOKE] verify Navigation and data loading
-*/
-test
-    .meta({testType: 'smoke', UITest: 'true', testGoal: 'Navigation'})
-    ('Navigation 1', async t =>{
-        //Log in as admin
-        await Davinci.Login.Email.SetText(Davinci.getUser());
-        await Davinci.Login.Next.Click();
-
-        await Davinci.Login.Password.SetText(Davinci.getPassword());
-        await Davinci.Login.Database.SelectByText(Davinci.getDatabase());
-        await Davinci.Login.Login.Click();
-        
-        // if the Select warehouse dialog is displayed then close it
-        if( await Dialogs.WareHouse.IsVisible() )
-        {
-            await Dialogs.WareHouse.selectWarehouse.SelectByText("<1> WHSE1");
-            await Dialogs.WareHouse.OK.Click();
-            await t.expect(await Dialogs.WareHouse.Exists()).notOk("WareHouse dialog is still displayed");
-        }
-
-        //within the menu go to Consolidated Bill of Lading
-        await Menu.ShippingOrders.GoTo();
-        //Verify the table has data displayed
-        await t.expect(Selector('tr.k-grid-norecords').exists).notOk("There is no data retreived from the DB!");
-    });
