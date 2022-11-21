2ab4a5c834ddb7dc645260d081cf7ece11839ee3
msanabria-wdg <msanabria@wdgcorp.com>
2022-11-21 11:01:29 -0400

    PD-932 updating item skus tab

---
+++ src/davinci-tabs/items-skus/dialog/itemSkus-Search.ts
@@ -0,0 +1,27 @@
+import { Button, Combobox, TextBox } from "../../../basic-objects";
+import { ComboboxSearch } from "../../../basic-objects/combobox-Search";
+import { BaseDialog } from "../../../page-object/dialogs/baseDialog";
+
+export class ItemSkus_CreateDialog extends BaseDialog{
+
+    public Account: ComboboxSearch;
+    public SKU: TextBox;
+    public ItemCode: TextBox;
+    public Description: TextBox;
+    
+    public Clear : Button;
+    public Search: Button;
+
+    constructor(selector: Selector){
+        super(selector);
+
+        const searchSelector = this._container.find('span').withText('Account');
+        this.Account = new ComboboxSearch(searchSelector.nextSibling(1));
+        this.SKU = new TextBox(this._container.find('span').withText('SKU').nextSibling('input'));
+        this.ItemCode = new TextBox(this._container.find('span').withText('Item Code').nextSibling('input'));
+        this.Description = new TextBox(this._container.find('span').withText('Description').nextSibling('input'));
+        
+        this.Search = new Button(this._container.find('button').withText('Search'));
+        this.Clear = new Button(this._container.find('button').withText('Clear'));
+    }
+}
